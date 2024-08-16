d3e769309729188743e7988f9fc26395b6d47045
Dmytro Popov <thedmitryp@ukr.net>
2024-08-16 19:58:11 +0100

    Fixed assets not being resolved in production

--- anthill_front/lib/src/shared/presentation/constraints/asset.dart
+++ anthill_front/lib/src/shared/presentation/constraints/asset.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/foundation.dart';
+
 enum Asset {
   logo('images/h4u_logo.png');
 
   final String _assetName;
 
-  String get assetName => 'assets/$_assetName';
+  String get assetName {
+    final path = 'assets/$_assetName';
+
+    if (kDebugMode) {
+      return path;
+    }
+
+    return 'assets/$path';
+  }
 
   const Asset(this._assetName);
 }
