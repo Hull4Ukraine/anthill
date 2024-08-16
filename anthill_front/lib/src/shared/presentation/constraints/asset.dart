339d5495fa8b3a0af9925173e733e3716b28376a
Dmytro Popov <77672371+mitryp@users.noreply.github.com>
2024-08-16 20:03:48 +0100

    Localization and customization update (#1)
    * Implemented localization (#36)
    * Updated the browser tab appearance
    * Fixed assets not being resolved in production

--- anthill_front/lib/src/shared/presentation/constraints/asset.dart
+++ anthill_front/lib/src/shared/presentation/constraints/asset.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/foundation.dart';
+
 enum Asset {
   logo('images/h4u_logo.png');
 
-  final String assetName;
+  final String _assetName;
 
-  const Asset(this.assetName);
+  String get assetName {
+    final path = 'assets/$_assetName';
+
+    if (kDebugMode) {
+      return path;
+    }
+
+    return 'assets/$path';
+  }
+
+  const Asset(this._assetName);
 }
